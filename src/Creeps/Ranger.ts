405a00b5e64094f226bbc3d6db3028b2400a33e4
Emin Khateeb <emin@emin.ch>
2019-07-28 00:53:23 +0200

    now moving to enemies

--- src/Creeps/Ranger.ts
+++ src/Creeps/Ranger.ts
@@ -2,8 +2,16 @@ export default (creep: Creep) => {
   const closestEnemy = creep.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
 
   if (!closestEnemy) {
+    const flag = creep.pos.findClosestByRange(FIND_FLAGS);
+    if (!flag) {
+      return;
+    }
+
+    creep.moveTo(flag);
     return;
   }
 
-  creep.rangedAttack(closestEnemy);
+  if (creep.attack(closestEnemy) === ERR_NOT_IN_RANGE) {
+    creep.moveTo(closestEnemy);
+  }
 };
