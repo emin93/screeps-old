9f889c4eb5af0c4806bac44d0fd2190a23514dd8
Emin Khateeb <emin@emin.ch>
2019-07-28 18:29:08 +0200

    builders can now also be harvesters if they have nothing to do

--- src/Creeps/Workers/Builder.ts
+++ src/Creeps/Workers/Builder.ts
@@ -1,4 +1,5 @@
 import { BaseCreepMemory, workerMoveOpts } from '../Base';
+import harvest from './Harvester';
 
 export interface BuilderMemory extends BaseCreepMemory {
   isBuilding: boolean;
@@ -18,7 +19,12 @@ export default (creep: Creep) => {
   if (memory.isBuilding) {
     const targets = creep.room.find(FIND_CONSTRUCTION_SITES);
 
-    if (targets.length && creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
+    if (!targets.length) {
+      harvest(creep);
+      return;
+    }
+
+    if (creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
       creep.moveTo(targets[0], workerMoveOpts);
     }
 
