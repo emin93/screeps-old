e76b4dc5bb199ec49e5274c014f096defb0f7d65
Emin Khateeb <emin@emin.ch>
2019-07-28 18:18:04 +0200

    optimized creep renewing

--- src/Utils/CreepRenewer.ts
+++ src/Utils/CreepRenewer.ts
@@ -1,40 +1,34 @@
 import { BaseCreepMemory } from '../Creeps/Base';
 import env from '../env';
 
-export default () => {
-  const renewableCreeps = Object.keys(Game.creeps)
+export default () =>
+  Object.keys(Game.creeps)
     .map(name => Game.creeps[name])
-    .filter(creep => {
+    .forEach(creep => {
       if (!creep.ticksToLive) {
-        return false;
+        return;
       }
 
       const memory = <BaseCreepMemory>creep.memory;
 
-      return creep.ticksToLive <= 50 || memory.isRenewing;
+      if (!memory.isRenewing && creep.ticksToLive <= 50) {
+        memory.isRenewing = true;
+      }
+
+      if (!memory.isRenewing) {
+        return;
+      }
+
+      const spawn = Game.spawns[env.spawnName];
+
+      const response = spawn.renewCreep(creep);
+
+      if (response === ERR_NOT_IN_RANGE) {
+        creep.moveTo(spawn.pos);
+        return;
+      }
+
+      if (response === OK && creep.ticksToLive >= 300) {
+        memory.isRenewing = false;
+      }
     });
-
-  if (!renewableCreeps.length) {
-    return;
-  }
-
-  const creep = renewableCreeps[0];
-  const memory = <BaseCreepMemory>creep.memory;
-
-  if (!memory.isRenewing) {
-    memory.isRenewing = true;
-  }
-
-  const spawn = Game.spawns[env.spawnName];
-
-  const response = spawn.renewCreep(creep);
-
-  if (response === ERR_NOT_IN_RANGE) {
-    creep.moveTo(spawn.pos);
-    return;
-  }
-
-  if (response === OK && creep.ticksToLive && creep.ticksToLive >= 300) {
-    memory.isRenewing = false;
-  }
-};
